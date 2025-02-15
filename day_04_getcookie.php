048d17b32008217bb57a91f95df60ef635b4aeeb
Khushigupta-23 <khushi75gupta8811@gmail.com>
2025-02-15 20:18:58 +0530

    Updated Day 4 files

--- day_04_getcookie.php
+++ day_04_getcookie.php
@@ -1,4 +1,5 @@
 <?php
+    // Before run this page in browser first run "cookie.php" in the browser.
     $cat = $_COOKIE['category'];
     echo "The list of $cat is here";
 ?>
--- day_04_table.php
+++ day_04_table.php
@@ -1,4 +1,4 @@
-<?php
+day_05<?php
     // Here i make a simple table using loop and use it to show the working of 'require' and 'include'.
     echo "<h3><u>Table of 5</u></h3>";
     for($i=1;$i<=10;$i++){
